5a5631172ce3a1e674bd2d71d129dfa1d6bb0ff6
Joris Pinkse <pinkse@gmail.com>
2022-07-01 17:04:23 -0400

    case

--- src/common/types/nodesweights.jl
+++ src/common/types/nodesweights.jl
@@ -92,7 +92,7 @@ function BothIntegrators( microIntegrator :: MicroIntegrator{T} ) where {T<:Flt}
 end
 
 function BothIntegrators( macroIntegrator :: MacroIntegrator{T} ) where {T<:Flt}
-    return BothIntegrators( DefaultmicroIntegrator{T}, macroIntegrator )
+    return BothIntegrators( DefaultMicroIntegrator{T}, macroIntegrator )
 end
 
 function BothIntegrators( T = F64 ) 
