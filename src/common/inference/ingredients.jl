2d2640abe0881b7a4596442663232c766b42746c
Joris Pinkse <pinkse@gmail.com>
2022-07-03 12:10:01 -0400

    negligible

--- src/common/inference/ingredients.jl
+++ src/common/inference/ingredients.jl
@@ -17,7 +17,6 @@ dimM( ii :: Ingredients ) = length( ii.ranges )
 
 function VarianceSum( X :: AA2{T}, ξ :: AA1{T}, Y :: AA2{T}, ::Val{:homo} )  where {T<:Flt}
     @ensure size(X,1) == size( Y, 1 ) == length( ξ )  "size mismatch" 
-    println( "mean = ", mean( ξ ))
     σξ2 = sum( ξ[i]^2 for i ∈ eachindex(ξ) ) / length( ξ )
     return σξ2 * X'Y
 end
