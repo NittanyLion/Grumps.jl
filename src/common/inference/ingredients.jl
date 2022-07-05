fc9992f35623384ccadec856465e200339b6523b
Joris Pinkse <pinkse@gmail.com>
2022-07-05 15:10:10 -0400

    removed duplication

--- src/common/inference/ingredients.jl
+++ src/common/inference/ingredients.jl
@@ -101,12 +101,6 @@ function VarianceSum( X :: AA2{T}, ξ :: AA1{T}, Y :: AA2{T}, ::Val{:homo} )  wh
     return σξ2 * X'Y
 end
 
-function VarianceSum( X :: AA2{T}, ξ :: AA1{T}, Y :: AA2{T}, ::Val{:homo} )  where {T<:Flt}
-    @ensure size(X,1) == size( Y, 1 ) == length( ξ )  "size mismatch" 
-    σξ2 = sum( ξ[i]^2 for i ∈ eachindex(ξ) ) / length( ξ )
-    return σξ2 * X'Y
-end
-
 
 function VarianceSum( X :: AA2{T}, ξ :: AA1{T}, Y :: AA2{T}, ::Val{:hetero} )  where {T<:Flt}
     @ensure size(X,1) == size( Y, 1 ) == length( ξ )  "size mismatch" 
