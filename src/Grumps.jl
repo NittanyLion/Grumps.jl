59dc8c154759788442e0fe90e776176331294bf3
Joris Pinkse <pinkse@gmail.com>
2022-07-18 07:43:06 -0400

    changed header

--- src/Grumps.jl
+++ src/Grumps.jl
@@ -18,13 +18,16 @@ function __init__()
     for 𝓁 ∈ readlines( "$(@__DIR__)/splash.txt" )  printstyled( "$𝓁\n"; bold = true, color=:blink  ) end
     println()
     printstyled( " Please note:\n"; bold = true, color=:green )
-    println(" 1: read the manual")
+    println(" 1: read the manual on github")
     println(" 2: this is a preliminary version")
     println(" 3: still need to put in designed quadrature")
     println(" 4: does not do CUDA")
     println(" 5: uses multithreading, but no distributed computing")
-    println(" 6: does not do multinomial logit")
-    println(" 7: please direct all questions/suggestions/comments to Joris Pinkse at joris@psu.edu\n\n")
+    println(" 6: several methods will be added soon")
+    println(" 7: because of the way the random numbers for MC integration are drawn / multithreading, successive optimizations with the same data may not produce the exact same results")
+    println(" 8: if you see unexpected behavior, please send me code and data so that I can replicate")
+    println(" 9: please direct all questions/suggestions/comments to Joris Pinkse at joris@psu.edu\n\n")
+
 end
 
 end
