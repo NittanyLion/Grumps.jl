87781bd72fa14b39b7a7ba246269ebf82e6e97a3
Joris Pinkse <pinkse@gmail.com>
2023-05-16 19:02:28 -0400

    doc

--- src/common/types/options.jl
+++ src/common/types/options.jl
@@ -67,13 +67,13 @@ Both this method and the one described below specify how Grumps should store its
     DataOptions(
         VarξInput   :: VarξInput{T},
         VarξOutput  :: VarξOutput,
-        micromode   :: Symbol,
-        macromode   :: Symbol,
-        balance     :: Symbol,
-        id          :: Symbol    
+        micromode   :: Symbol = :Hog,
+        macromode   :: Symbol = :Ant,
+        balance     :: Symbol = :micro,
+        id          :: Symbol = :Grumps   
     )
 
-Both this method and the one described above specify how Grumps should store its data and what it should store.  This one is both more complex and more flexible.  The first three options are best set to their defaults, unless you know what it is you're doing.  The **id** option is used to extend Grumps with other data constructions.  **VarξInput** is the variance matrix to be used in the penalty term weight matrix construction.  This should be a J*J matrix where J is the number of products across all markets.  Acceptable types for **VarξInput** include UniformScaling{T} (e.g. 1.0 * I ) and AbstractMatrix{T} (sparse matrix is recommended to conserve space, but a dense matrix is allowed).  **VarξOutput** is used to indicate what assumptions on the variance of ξ must be produced in the solution, which can subsequently be used as an input in a second stage if desirable; options are *VarξHomoskedastic*, *VarξHeteroskedastic*, *VarξClustering*, and *VarξUser*.
+Both this method and the one described above specify how Grumps should store its data and what it should store.  This one is both more complex and more flexible.  The **micromode**, **macromode**, and **balance** arguments are best kept at their defaults, unless you know what it is you're doing.  The **id** option is used to extend Grumps with other data constructions.  **VarξInput** is the variance matrix to be used in the penalty term weight matrix construction.  This should be a J by J matrix where J is the number of products across all markets.  Acceptable types for **VarξInput** include UniformScaling{T} (e.g. 1.0 * I ) and AbstractMatrix{T} (sparse matrix is recommended to conserve space, but a dense matrix is allowed).  **VarξOutput** is used to indicate what assumptions on the variance of ξ must be produced in the solution, which can subsequently be used as an input in a second stage if desirable; options are *VarξHomoskedastic*, *VarξHeteroskedastic*, *VarξClustering*, and *VarξUser*.
 """
 DataOptions( x...; kwargs... ) = GrumpsDataOptions( x...; kwargs... )
 
