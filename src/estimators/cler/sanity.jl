34b587b5cac4dbee423f8729c2fbe35c6cf64cb4
Joris Pinkse <pinkse@gmail.com>
2023-08-19 17:57:44 -0400

    removed advisory

--- src/estimators/cler/sanity.jl
+++ src/estimators/cler/sanity.jl
@@ -6,7 +6,7 @@ function CheckSanitySpecific( e::GrumpsPenalized, d :: Data{T}, o :: Optimizatio
     end
     @ensure dimmom( d ) > dimβ( d ) "your product level moments are underidentified"
     @ensure ( size( d.plmdata.𝒦, 1 ) > 0 && size( d.plmdata.𝒦, 2 ) > 0 )  "matrix 𝒦  has dimension 0: this means that your product level moments do not provide identification"
-    advisory( "this is the full version of CLER:\nthe cheap version has the same limit distribution,\nbut is less expensive computationally" )
+    # advisory( "this is the full version of CLER:\nthe cheap version has the same limit distribution,\nbut is less expensive computationally" )
     return e
 end
     
