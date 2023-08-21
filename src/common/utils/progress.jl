3ac0d094f5057a0589570478a7a0720fec0fa849
Joris Pinkse <pinkse@gmail.com>
2023-08-20 22:10:52 -0400

    Mac progressbar

--- src/common/utils/progress.jl
+++ src/common/utils/progress.jl
@@ -15,12 +15,20 @@ function UpdateProgressBar( ρ )
     s = done( ρ2 )
     c =  done( 5ρ2 - s + 1.0 )
     y,x = displaysize( stdout )
-    Ansillary.Cursor.save() do 
-        # y, x = Ansillary.Screen.displaysize()
-        Ansillary.Cursor.move!( Ansillary.Cursor.Coordinate(1,x-5) )
-        printstyled( progressshape[1]^(5-ℓ); color = progresscolor[1] )
-        printstyled( progressshape[s]; color = progresscolor[c] )
-        printstyled( (progressshape[5])^(ℓ-1); color = progresscolor[5] )
-    end 
+    # Ansillary.Cursor.save() do 
+    #     # y, x = Ansillary.Screen.displaysize()
+    #     Ansillary.Cursor.move!( Ansillary.Cursor.Coordinate(1,x-5) )
+    #     printstyled( progressshape[1]^(5-ℓ); color = progresscolor[1] )
+    #     printstyled( progressshape[s]; color = progresscolor[c] )
+    #     printstyled( (progressshape[5])^(ℓ-1); color = progresscolor[5] )
+    # end     
+    Ansillary.Screen.raw() do 
+        Ansillary.Cursor.checkpoint() do
+            Ansillary.Cursor.move!( Ansillary.Cursor.Coordinate(1,x-5) )
+            printstyled( progressshape[1]^(5-ℓ); color = progresscolor[1] )
+            printstyled( progressshape[s]; color = progresscolor[c] )
+            printstyled( (progressshape[5])^(ℓ-1); color = progresscolor[5] )
+        end 
+    end
     return nothing
 end
